62784c81f91995ce0d8b76e79abdb9f6ed2298a8
chenjiang <chenjiang@ysstech.com>
2024-08-06 17:51:19 +0800

    first commit

---
+++ app/post/page.tsx
@@ -0,0 +1,10 @@
+import Header from '@/components/Header';
+
+export default function Post() {
+  return (
+    <main className='min-h-screen  p-24'>
+      文章首页
+      <Header />
+    </main>
+  );
+}
---
+++ components/Header/index.tsx
@@ -0,0 +1,3 @@
+export default function Post() {
+  return <h1>通用头部</h1>;
+}
