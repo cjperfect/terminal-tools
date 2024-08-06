922e5d0e9e53f6570c3df3cdc16357dea4c9d609
ChenJiang <1982145923@qq.com>
2024-08-06 22:55:04 +0800

    update config

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,13 +1,9 @@
+import BaseHeader from '@/components/BaseHeader';
+
 export default function Post() {
   return (
     <main className='min-h-screen p-24'>
-      <ul className='list-disc hover:list-disc'>
-        <li>Now1 this is a story all about how, my life got flipped-turned upside down</li>
-        <li>Now2 this is a story all about how, my life got flipped-turned upside down</li>
-        <li>Now3 this is a story all about how, my life got flipped-turned upside down</li>
-        <li>Now4 this is a story all about how, my life got flipped-turned upside down</li>
-        <li>Now5 this is a story all about how, my life got flipped-turned upside down</li>
-      </ul>
+      <BaseHeader />
     </main>
   );
 }
