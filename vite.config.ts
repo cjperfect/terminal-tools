9e1d9f258188d1f15ece2b158b272df6fc7c415a
cjperfect <chenjiang@ysstech.com>
2024-09-12 09:48:20 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { resolve } from "path";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "docs",
+    outDir: "./docs",
   },
   resolve: {
     extensions: [".ts", ".tsx"],
